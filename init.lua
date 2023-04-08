73e4984de4be085e459fa87a8bb5d5c57a01328c
cchung4010 <tokyo.calvin@gmail.com>
2023-04-08 13:48:35 +0900

    changed colorscheme to 'codedark'

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "rubber-enhanced",
+  colorscheme = "codedark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
